0e3bb2c6926bd15c80a5c769773c2cb95d9a2539
MyazinAS <amyazinfghj@gmail.com>
2023-01-31 17:30:30 +0300

    added main page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// window.onscroll = function showHeader() {
+//   const header = document.querySelector(".header__nav");
+//   const navBorder = document.querySelector(".nav");
+
+//   if (window.pageYOffset) {
+//     header.classList.add("nav__scroll");
+//   } else header.classList.remove("nav__scroll");
+
+//   if (window.pageYOffset) {
+//     navBorder.classList.add("home__txt-new");
+//   } else navBorder.classList.remove("home__txt-new");
+// };
